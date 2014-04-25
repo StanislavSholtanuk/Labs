d6b81c671b0ea3739ee4b27ad136c7f04bc56aaf
StanislavSholtanuk <ctaclllojitahl0k@gmail.com>
2014-04-25 11:43:49 +0300

    Task #10
    Without the y variable

--- Lab_work_10/main.cpp
+++ Lab_work_10/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main()
 {
 	double s = 0;
-	int n, m; double x, y = 0;
+	int n, m; double x;
 	cin >> m >> n >> x;
 
 	// formulae for calculating the s variable in C++:
@@ -33,15 +33,15 @@ int main()
 			add n, 1;                n = i + 1
 			fiadd n;                 2 * m + i + 1
 			fdiv st(1), st(0);       1 / i / (2 * m + i + 1) = 1 / (i * (2 * m + i + 1))
-			fstp y;                  deleting the register with the following expression : 2 * m + i + 1
+			fstp st(0);                  deleting the register with the following expression : 2 * m + i + 1
 			fadd st(1), st(0);       s += 1 / (i * (2 * m + i + 1))
-			fstp y;                  deleting the register with the following expression : 1 / (i * (2 * m + i + 1))
+			fstp st(0);                  deleting the register with the following expression : 1 / (i * (2 * m + i + 1))
 
 			fild n;                  n = i + 1 (see the 33th line)
 			fimul n;                 n * n = (i + 1) * (i + 1)
 			fdiv x;                  (i + 1) * (i + 1) / x
 			fmul st(1), st(0);       s *= (i + 1) * (i + 1) / x
-			fstp y;                  deleting the register with the following expression : (i + 1) * (i + 1) / x
+			fstp st(0);                  deleting the register with the following expression : (i + 1) * (i + 1) / x
 
 			loop calc
 
